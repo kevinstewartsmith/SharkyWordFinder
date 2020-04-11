a89cb1348f45f821e171d2f2d3d29d1aba7f5fe7
kssmith9 <kssmith9@ncsu.edu>
2020-04-11 23:54:04 +0700

    Initial Commit

---
+++ SharkyWordFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SharkyWordFinder
+//
+//  Created by Kevin Smith on 4/11/20.
+//  Copyright © 2020 Kevin Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
